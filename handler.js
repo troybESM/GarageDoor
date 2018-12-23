c91901f008ededf40247f67162867da2919878dc
troy <troybrewer1167@gmail.com>
2018-12-23 12:05:22 -0500

    Added status lambda

--- handler.js
+++ handler.js
@@ -1,11 +1,32 @@
 'use strict';
+var MyQ = require('myq-api');
+var email = process.env.USER
+var password = process.env.PASS
+var account = new MyQ(email, password);
 
-module.exports.hello = async (event, context) => {
+module.exports.status = async (event, context) => {
+  await account.login()
+  .then(function (result) {
+    console.log(result);
+  }).catch(function (err) {
+    console.error(err);
+  });
+
+  let myDevices =await account.getDevices([3, 15, 17])
+  .then(function (result) {
+    console.log(result);
+    return result;
+  }).catch(function (err) {
+    console.error(err);
+    return err;
+  });
+  
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'These are the devices associated with your account',
       input: event,
+      devices: myDevices
     }),
   };
 
